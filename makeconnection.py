20b0e52b9eda92752224342237de1ca561d073b0
isavin <78101592+savinjenia@users.noreply.github.com>
2025-09-05 15:50:22 +0000

    Update makeconnection with my creds

--- makeconnection.py
+++ makeconnection.py
@@ -1,20 +1,14 @@
-import sys
-from snowflake.snowpark import Session
-
-# If you are running this code in Github code spaces you don't need to execute below command,
-#sys.path.append('/Users/pradeep/Downloads/Udemy_course_videos/course_2_assignments/Snowpark_pipeline/')
-
 from generic_code import code_library
 
 # Make connection and create Snowpark session.
 # Please mention your snowflake account credentials below,
-connection_parameters = {"account":"ovmyonn-cp58207", \
-"user":"pradeep", \
-"password": "Abcd067$", \
-"role":"ACCOUNTADMIN", \
-"warehouse":"COMPUTE_WH", \
-"database":"DEMO_DB", \
-"schema":"PUBLIC" \
+connection_parameters = {"account":"EFPKHYG-KT18285",
+                         "user":"ISAVIN",
+                         "password": "8ZRu7mhptF2Q37D$",
+                         "role":"ACCOUNTADMIN",
+                         "warehouse":"COMPUTE_WH",
+                         "database":"DEMO_DB",
+                         "schema":"PUBLIC"
 }
 
 # Create connection with snowflake and return the session
